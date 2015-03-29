2b121015f0135b050c87494f2c13fabfae167dab
Dean Perry <dean@deanpcmad.com>
2015-03-29 02:03:48 +0100

    cleanup and started to add docs & comments

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 **Currently in development**
 
 This module helps with including PayPal within your Shoppe application. The information below explains how to get this module installed within your Rails application.
+
+Add the gem to your Gemfile and run `bundle install`
+
+`gem "shoppe-paypal"`
--- lib/shoppe/paypal.rb
+++ lib/shoppe/paypal.rb
@@ -22,31 +22,33 @@ module Shoppe
       end
       
       def setup
+        # Set the configuration
         Shoppe.add_settings_group :paypal, [:paypal_client_id, :paypal_secret_id, :currency]
 
-        # Set the configuration
+        # Require the Paypal library
         require 'paypal-sdk-rest'
         
         include PayPal::SDK::REST
 
+        # Configure the Paypal library
         PayPal::SDK.configure({
           mode:          (Rails.env.production? ? "live" : "sandbox"),
           client_id:     Shoppe.settings.paypal_client_id,
           client_secret: Shoppe.settings.paypal_secret_id
         })
 
-        # When an order is accepted, attempt to capture the payment
+        # When an order is accepted, attempt to capture/execute the payment
         Shoppe::Order.before_acceptance do
           self.payments.where(confirmed: false, method: "PayPal").each do |payment|
-            # begin
+            begin
               payment.paypal_payment.execute(payer_id: payment.order.properties["paypal_payer_id"])
               payment.update_attribute(:confirmed, true)
-            
-            # rescue ::Stripe::CardError
-              # raise Shoppe::Errors::PaymentDeclined, "Payment ##{payment.id} could not be captured by Stripe. Investigate with Stripe. Do not accept the order."
-            # end
+            rescue
+              raise Shoppe::Errors::PaymentDeclined, "Payment ##{payment.id} could not be captured by PayPal. Investigate with PayPal. Do not accept the order."
+            end
           end
         end
+
       end
 
     end
